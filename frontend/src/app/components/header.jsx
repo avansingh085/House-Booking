f7e1bc7ec57a0ff720a9306d3d677850b5ea4df1
Avan Singh <avansingh085@gmail.com>
2025-05-04 18:27:31 +0530

    update and commit

--- frontend/src/app/components/header.jsx
+++ frontend/src/app/components/header.jsx
@@ -33,13 +33,13 @@ function Header() {
         </div>
 
         <nav className="hidden md:flex gap-6">
-          <Link href="/" className="font-bold text-2xl">Home</Link>
-          <Link href={`/pages/${isAuthenticated ? 'history' : 'login'}`} className="font-bold text-2xl">History</Link>
-          <Link href={`/pages/${isAuthenticated ? 'listHouse': 'login'}`} className="font-bold text-2xl">List House</Link>
-          <Link href={`/pages/${isAuthenticated ? 'booking': 'login'}`} className="font-bold text-2xl">My Booking</Link>
+          <Link href="/" className="font-bold text-xl">Home</Link>
+          <Link href={`/pages/${isAuthenticated ? 'history' : 'login'}`} className="font-bold text-xl">History</Link>
+          <Link href={`/pages/${isAuthenticated ? 'listHouse': 'login'}`} className="font-bold text-xl">List House</Link>
+          <Link href={`/pages/${isAuthenticated ? 'booking': 'login'}`} className="font-bold text-xl">My Booking</Link>
           {
-            isAuthenticated ? <Link href="/pages/profile" className="font-bold text-2xl">profile</Link> :
-            <Link href="/pages/login" className="font-bold text-2xl">Login</Link>
+            isAuthenticated ? <Link href="/pages/profile" className="font-bold text-xl">profile</Link> :
+            <Link href="/pages/login" className="font-bold text-xl">Login</Link>
           }
          
          
@@ -60,16 +60,16 @@ function Header() {
         </div>
         <ul className="flex flex-col p-4 space-y-4 text-gray-800">
 
-          <li><Link href="/" onClick={() => setSidebarOpen(false)} className="font-bold text-2xl">Home</Link></li>
-          <li><Link href={`/pages/${isAuthenticated ? 'history' : 'login'}`} onClick={() => setSidebarOpen(false)} className="font-bold text-2xl">History</Link></li>
-          <li><Link href={`/pages/${isAuthenticated ? 'listHouse': 'login'}`} onClick={() => setSidebarOpen(false)} className="font-bold text-2xl">List House</Link></li>
+          <li><Link href="/" onClick={() => setSidebarOpen(false)} className="font-bold text-xl">Home</Link></li>
+          <li><Link href={`/pages/${isAuthenticated ? 'history' : 'login'}`} onClick={() => setSidebarOpen(false)} className="font-bold text-xl">History</Link></li>
+          <li><Link href={`/pages/${isAuthenticated ? 'listHouse': 'login'}`} onClick={() => setSidebarOpen(false)} className="font-bold text-xl">List House</Link></li>
             <li>
-            <Link href={`/pages/${isAuthenticated ? 'booking': 'login'}`} onClick={() => setSidebarOpen(false)} className="font-bold text-2xl">My Booking</Link>
+            <Link href={`/pages/${isAuthenticated ? 'booking': 'login'}`} onClick={() => setSidebarOpen(false)} className="font-bold text-xl">My Booking</Link>
             </li>
            <li>
            {
-            isAuthenticated ? <Link href="/pages/profile" className="font-bold text-2xl" onClick={() => setSidebarOpen(false)}>profile</Link> :
-            <Link href="/pages/login" className="font-bold text-2xl" onClick={() => setSidebarOpen(false)}>Login</Link>
+            isAuthenticated ? <Link href="/pages/profile" className="font-bold text-xl" onClick={() => setSidebarOpen(false)}>profile</Link> :
+            <Link href="/pages/login" className="font-bold text-xl" onClick={() => setSidebarOpen(false)}>Login</Link>
           }
            </li>
         </ul>
